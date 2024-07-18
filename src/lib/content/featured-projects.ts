a05e22ec2bd0f21ed16a3ff6a733772770a08627
suntzu62 <gabrielfootze@gmail.com>
2024-07-17 23:33:51 -0300

    Update projects

--- src/lib/content/featured-projects.ts
+++ src/lib/content/featured-projects.ts
@@ -6,7 +6,7 @@ const featuredProjectsSection: FeaturedProjectsSectionType = {
   projects: [
     {
       id: getId(),
-      name: 'GetLifeGuard',
+      name: 'E-commerce',
       description: 'E-commerce moderno com tecnologias atualizadas desde adição de carrinho .',
       tasks:
         "Como freelancer, entendi os requisitos do cliente e executei um plano para desenvolver e projetar uma plataforma de e-commerce, incluindo UI frontend, configuração backend com banco de dados, APIs e integração com plataformas de pagamento.",
@@ -23,23 +23,23 @@ const featuredProjectsSection: FeaturedProjectsSectionType = {
     },
     {
       id: getId(),
-      name: 'Velvi Matrimony',
+      name: 'Blog News',
       description:
-        'A platform that redefines the way individuals find their life partners.',
+        'Modelo Next.js limpo e moderno para revistas, notícias e blogs.',
       tasks:
-        'As a freelancer, I developed this project by planning, communicating with the client, gathering feedback, implementing UI & pages, and integrating real-time data fetching with filtering functionality.',
-      url: 'https://velvi.vercel.app/',
-      img: 'https://i.postimg.cc/tRS4GvzJ/Screenshot-2024-02-27-at-5-48-49-PM.png',
+        '',
+      url: 'https://blog-tau-green.vercel.app/',
+      img: '/download.png',
       tags: ['NextJs', 'TypeScript', 'TailwindCSS', 'APIs'],
     },
     {
       id: getId(),
-      name: 'Integra',
-      description: 'An AI-Powered Leads Generation Platform.',
+      name: 'Hotel',
+      description: 'Projeto de hotel&spa com paleta de cores combinando e com efeitos modernos.',
       tasks:
-        'I strategically planned project milestones, led a cross-functional team, established a design system, developed the website, and integrated Stripe payments with secure authentication.',
-      url: 'https://www.integraleads.xyz/',
-      img: 'https://user-images.githubusercontent.com/68834718/279476369-2f69466a-71db-4da0-9afd-04f8f0efb621.jpeg',
+        '',
+      url: 'https://front-hotel.vercel.app/',
+      img: '/hotel.jpeg',
       tags: ['AI/ML', 'NextJs', 'TypeScript', 'TailwindCSS', 'Express'],
     },
     {
@@ -50,6 +50,16 @@ const featuredProjectsSection: FeaturedProjectsSectionType = {
         'Implemented NextAuth authentication, integrated Mailchimp for newsletter subscription, developed a custom hook for dark mode, created UI components in the Dashboard, and used Notion as a CMS for the blogs page.',
       url: 'https://www.drafton.io/',
       img: 'https://i.postimg.cc/4N6dsGcm/Screenshot-2024-06-17-at-11-05-22-PM.png',
+      tags: ['Open AI', 'NextJs', 'TypeScript', 'TailwindCSS', 'Prisma'],
+    },
+    {
+      id: getId(),
+      name: 'Portfolio',
+      description: '.',
+      tasks:
+        'Feito com tecnologias modernas com grande interatividade ao momento que rolando o mouse para baixo as informações vão aparecendo.',
+      url: 'https://master-portfolio-neon.vercel.app/home',
+      img: '/port.jpeg',
       tags: ['Open AI', 'NextJs', 'TypeScript', 'TailwindCSS', 'Prisma'],
     },
   ],
